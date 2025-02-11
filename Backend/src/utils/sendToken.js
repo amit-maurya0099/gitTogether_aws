2713f55306508dc198273851dd797cafabdb955b
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2025-02-11 11:46:35 +0530

    cookies for production setup

--- Backend/src/utils/sendToken.js
+++ Backend/src/utils/sendToken.js
@@ -6,8 +6,8 @@ const sendToken=async(user,res,statusCode,message)=>{
     
     const options={
         httpOnly:true,
-        sameSite:"None",
-        secure:true,
+        sameSite:"Lax",
+        secure:false,
         expires:new Date(Date.now()+ 24*3600*1000)
     }
     res.status(statusCode).cookie("token",token,options).json({message,user});
