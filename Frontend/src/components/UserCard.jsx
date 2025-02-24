aa355969c79499493286536a5c9d0cca7750542d
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2025-02-24 19:36:36 +0530

    change in ui of user card

--- Frontend/src/components/UserCard.jsx
+++ Frontend/src/components/UserCard.jsx
@@ -30,7 +30,7 @@ const UserCard = ({user}) => {
      duration:0.5,
      
     }}
-    className="card bg-base-300 w-[85%]   md:w-[20%] h-[70%] md:h-[80%] shadow-sm text-white  ">
+    className="card bg-base-300 w-[85%]   md:w-[20%] h-[70%] md:h-[80%] shadow-sm text-white rounded-xl  ">
   <div className='h-[50%]'>
     <img
       src={user?.avatar.url}
