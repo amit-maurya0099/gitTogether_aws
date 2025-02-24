c4930f59052dbaacf6ed9b9350467aca698a5cff
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2025-02-24 22:43:13 +0530

    loader changed

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import LoadingImg from "../assets/loadingImg.gif"
+import Lottie from 'lottie-react'
+import loadingAnimation from "../Animations/loaderAnimation.json"
 
 const Loader = () => {
   return (
     <div className='h-full w-full flex items-center justify-center '>
-       <img src={LoadingImg} alt="/loader" className='object-cover h-[40%]'></img>
+       {/* <img src={LoadingImg} alt="/loader" className='object-cover h-[40%]'></img> */}
+       <Lottie animationData={loadingAnimation} loop={true} 
+       style={{width:200 ,height:200}}></Lottie>
+
     </div>
   )
 }
